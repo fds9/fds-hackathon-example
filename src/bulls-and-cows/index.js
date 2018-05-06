3af6ddeaf38328704396fca6919f2043cf8e5fe8
Kim Seungha <seungha.me@gmail.com>
2018-05-07 02:04:07 +0900

    숫자야구 CSS

--- src/bulls-and-cows/index.js
+++ src/bulls-and-cows/index.js
@@ -4,10 +4,44 @@ const inputBoxEl = document.querySelector('.input-box');
 const tryButtonEl = document.querySelector('.input-box__try');
 const digitInputEls = document.querySelectorAll('.input-box__digit');
 const turnListEl = document.querySelector('.turn-list');
-const resetButtonEl = document.querySelector('.reset-button');
+const resetButtonEl = document.querySelector('.input-box__reset');
 const answerEl = document.querySelector('.answer');
 
 let game = new BullsAndCows();
+
+function appendTurnResult({digits, strike, ball, turn}) {
+  const itemEl = document.createElement('div');
+  itemEl.classList.add('turn-list__turn-item');
+
+  const digitsEl = document.createElement('div');
+  digitsEl.classList.add('turn-item__turn-digits');
+
+  const turnNumEl = document.createElement('div');
+  turnNumEl.classList.add('tur-item__num');
+  turnNumEl.textContent = `${turn}회`;
+  digitsEl.appendChild(turnNumEl);
+
+  digits.forEach(d => {
+    const digitEl = document.createElement('div');
+    digitEl.classList.add('turn-digits__digit');
+    digitEl.textContent = d;
+    digitsEl.appendChild(digitEl);
+  })
+
+  itemEl.appendChild(digitsEl);
+
+  const resultEl = document.createElement('div');
+  resultEl.classList.add('turn-item__turn-result');
+  const resultText = (
+    strike === 0 && ball === 0 ?
+    'OUT' :
+    `${ball}B ${strike}S`
+  );
+  resultEl.textContent = resultText;
+  itemEl.appendChild(resultEl);
+
+  turnListEl.appendChild(itemEl);
+}
 
 digitInputEls.forEach((el, index) => {
   el.addEventListener('keypress', e => {
@@ -29,14 +63,24 @@ inputBoxEl.addEventListener('submit', e => {
   e.preventDefault();
   const digits = Array.from(digitInputEls).map(el => el.value);
   const turnResultEl = document.createElement('div');
-  const turnResult = game.nextTurn(digits);
-  turnResultEl.textContent = digits.join() + ' : ' + JSON.stringify(turnResult); // FIXME
-  turnListEl.appendChild(turnResultEl);
+  const turnResult = {
+    digits,
+    ...game.nextTurn(digits)
+  };
+  appendTurnResult(turnResult);
   if (turnResult.clear || turnResult.fail) {
-    answerEl.textContent = game._answerDigits.join();
+    const answer = game._answerDigits.join(', ');
+    answerEl.textContent = (
+      turnResult.clear ?
+      `정답을 맞추셨습니다: ${answer}` :
+      `정답은 ${answer} 입니다.`
+    );
     tryButtonEl.setAttribute('disabled', '');
     resetButtonEl.focus();
   } else {
+    digitInputEls.forEach(el => {
+      el.value = '';
+    });
     digitInputEls[0].focus();
   }
 });
